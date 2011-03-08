e26b7a23dd55d6250cb928fde549849ce71e9aa2
Trystan <trystan.s@comcast.net>
2011-03-07 17:54:54 -0800

    added capture

--- src/System/IO/Silently.hs
+++ src/System/IO/Silently.hs
@@ -1,6 +1,6 @@
 
 module System.IO.Silently (
-  silently, hSilently
+  silently, hSilently, capture, hCapture
 ) where
 
 import GHC.IO.Handle
@@ -9,10 +9,12 @@ import Control.Exception (bracket)
 import System.Directory
 
 -- | Run an IO action while ignoring all output to stdout.
+-- This will create and delete a temp file in the current directory.
 silently :: IO a -> IO a
 silently = hSilently stdout
 
 -- | Run an IO action while ignoring all output to the given handle.
+-- This will create and delete a temp file in the current directory.
 hSilently :: Handle -> IO a -> IO a
 hSilently handle action = do
   oldHandle <- hDuplicate handle
@@ -22,3 +24,23 @@ hSilently handle action = do
                                        removeFile tmpFile)
           (\(_,       tmpHandle) -> do hDuplicateTo tmpHandle handle
                                        action)
+
+-- | Run an IO action while capturing all output to stdout.
+-- This will create and delete a temp file in the current directory.
+capture :: IO a -> IO (String, a)
+capture = hCapture stdout
+
+-- | Run an IO action while capturing all output to the given handle.
+-- This will create and delete a temp file in the current directory.
+hCapture :: Handle -> IO a -> IO (String, a)
+hCapture handle action = do
+  oldHandle <- hDuplicate handle
+  bracket (openTempFile "." "silently")
+          (\(tmpFile, tmpHandle) -> do hDuplicateTo oldHandle handle
+                                       hClose tmpHandle
+                                       removeFile tmpFile)
+          (\(tmpFile, tmpHandle) -> do hDuplicateTo tmpHandle handle
+                                       a <- action
+                                       hClose tmpHandle
+                                       str <- readFile tmpFile
+                                       return (str, a))
