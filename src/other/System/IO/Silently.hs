ad91745635749d7cd2b2f7454b870d593fad146b
Trystan <trystan.s@comcast.net>
2011-03-20 18:29:11 -0700

    use temp dir if available, otherwise current dir

--- src/other/System/IO/Silently.hs
+++ src/other/System/IO/Silently.hs
@@ -10,7 +10,7 @@ module System.IO.Silently (
 import GHC.IO.Handle (hDuplicate, hDuplicateTo)
 import System.IO (Handle, stdout, hClose, openTempFile)
 import Control.Exception (bracket)
-import System.Directory (removeFile)
+import System.Directory (removeFile,getTemporaryDirectory)
 
 -- | Run an IO action while preventing all output to stdout.
 -- This will, as a side effect, create and delete a temp file in the current directory.
@@ -30,17 +30,21 @@ hSilence handles action = do
                                        action)
 
 
+getTempOrCurrentDirectory :: IO String
+getTempOrCurrentDirectory = getTemporaryDirectory `Prelude.catch` (\ex -> return ".")
+
 -- | Run an IO action while preventing and capturing all output to stdout.
--- This will, as a side effect, create and delete a temp file in the current directory.
+-- This will, as a side effect, create and delete a temp file in the temp directory or current direcoty if there is no temp directory.
 capture :: IO a -> IO (String, a)
 capture = hCapture [stdout]
 
 -- | Run an IO action while preventing and capturing all output to the given handles.
--- This will, as a side effect, create and delete a temp file in the current directory.
+-- This will, as a side effect, create and delete a temp file in the temp directory or current direcoty if there is no temp directory.
 hCapture :: [Handle] -> IO a -> IO (String, a)
 hCapture handles action = do
   oldHandles <- mapM hDuplicate handles
-  bracket (openTempFile "." "capture")
+  tmpDir <- getTempOrCurrentDirectory
+  bracket (openTempFile tmpDir "capture")
           (\(tmpFile, tmpHandle) -> do sequence_ $ zipWith hDuplicateTo oldHandles handles
                                        hClose tmpHandle
                                        removeFile tmpFile)
--- src/unix/System/IO/Silently.hs
+++ src/unix/System/IO/Silently.hs
@@ -10,15 +10,13 @@ module System.IO.Silently (
 import GHC.IO.Handle (hDuplicate, hDuplicateTo)
 import System.IO (Handle, stdout, hClose, openTempFile, openFile, IOMode(..))
 import Control.Exception (bracket)
-import System.Directory (removeFile)
+import System.Directory (removeFile,getTemporaryDirectory)
 
 -- | Run an IO action while preventing all output to stdout.
--- This will, as a side effect, create and delete a temp file in the current directory.
 silence :: IO a -> IO a
 silence = hSilence [stdout]
 
 -- | Run an IO action while preventing all output to the given handles.
--- This will, as a side effect, create and delete a temp file in the current directory.
 hSilence :: [Handle] -> IO a -> IO a
 hSilence handles action = do
   oldHandles <- mapM hDuplicate handles
@@ -29,17 +27,22 @@ hSilence handles action = do
                              action)
 
 
+
+getTempOrCurrentDirectory :: IO String
+getTempOrCurrentDirectory = getTemporaryDirectory `Prelude.catch` (\ex -> return ".")
+
 -- | Run an IO action while preventing and capturing all output to stdout.
--- This will, as a side effect, create and delete a temp file in the current directory.
+-- This will, as a side effect, create and delete a temp file in the temp directory or current direcoty if there is no temp directory.
 capture :: IO a -> IO (String, a)
 capture = hCapture [stdout]
 
 -- | Run an IO action while preventing and capturing all output to the given handles.
--- This will, as a side effect, create and delete a temp file in the current directory.
+-- This will, as a side effect, create and delete a temp file in the temp directory or current direcoty if there is no temp directory.
 hCapture :: [Handle] -> IO a -> IO (String, a)
 hCapture handles action = do
   oldHandles <- mapM hDuplicate handles
-  bracket (openTempFile "." "capture")
+  tmpDir <- getTempOrCurrentDirectory
+  bracket (openTempFile tmpDir "capture")
           (\(tmpFile, tmpHandle) -> do sequence_ $ zipWith hDuplicateTo oldHandles handles
                                        hClose tmpHandle
                                        removeFile tmpFile)
